90893ef753eb0c10ea4c3a669f9a7aed02cf01fb
Oliver Widmer <oliverwidmer@Mac-mini-von-Oliver.local>
2026-03-20 11:13:47 +0100

    🗺️ Fix site URL → www.finpost.ch (sitemap korrekt)

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://kipost.ch',
+	site: 'https://www.finpost.ch',
 	integrations: [mdx(), sitemap()],
 });
